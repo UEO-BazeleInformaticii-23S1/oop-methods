a92f20dd175e3ac7cb41957dfccc471a7a38feb9
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-17 08:32:19 +0200

    method overloading usage

--- OopMethods/Program.cs
+++ OopMethods/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConsoleHelper.Print("test");
+
+            ConsoleHelper.Print("label", "test");
+
+            ConsoleHelper.Print("test", 3);
         }
     }
 }
