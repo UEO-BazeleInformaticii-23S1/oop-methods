d7abc480ff7ea7b0ab4def24d41920d978c630d5
Florin Lazar <florin.lazar.it2014@gmail.com>
2023-11-17 08:20:51 +0200

    initial project setup

---
+++ OopMethods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OopMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
